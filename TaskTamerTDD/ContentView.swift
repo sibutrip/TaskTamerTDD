338d55b9fb76b561c2029bc291ddd96c698d7df8
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2023-12-08 14:40:21 -0500

    Initial Commit

---
+++ TaskTamerTDD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskTamerTDD
+//
+//  Created by Cory Tripathy on 12/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskTamerTDD/TaskTamerTDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTamerTDDApp.swift
+//  TaskTamerTDD
+//
+//  Created by Cory Tripathy on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskTamerTDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
