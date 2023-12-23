38096edc38db98d5fa0f6c1f07f01fee4d869da9
Cory Sibu Tripathy <109297828+sibutrip@users.noreply.github.com>
2023-12-23 17:42:41 -0500

    test_sort_changesSortStatusToNewStatus

--- TaskTamerTDDTests/TaskManagerTests.swift
+++ TaskTamerTDDTests/TaskManagerTests.swift
@@ -12,31 +12,42 @@ import XCTest
 class TaskManager {
     var tasks = [TaskItem]()
     func sort(_ task: TaskItem, to sortStatus: SortStatus) { 
-        let taskToSort = tasks.first(where: {$0.id == task.id})
+        guard var taskToSort = tasks.first(where: { $0.id == task.id }) else {
+            return
+        }
+        taskToSort.sortStatus = sortStatus
+        var tasks = tasks.filter { $0.id != task.id }
+        tasks.append(taskToSort)
+        self.tasks = tasks
     }
 }
 
-//final class TaskManagerTests: XCTestCase {
-//    func test_sort_changesSortStatusToNewStatus() {
-//        let sut = makeSUT(withTasks: existingTasks())
-//        let taskToSort = sut.tasks.first!
-//        sut.sort(taskToSort, to: .afternoon)
-//        let sortedTask = sut.tasks.first!
-//        XCTAssertEqual(sortedTask.sortStatus, .afternoon)
-//    }
-//    
-//    // MARK: Helper Methods
-//    
-//    private func existingTasks() -> [TaskItem] {
-//        return [
-//            TaskItem(title: "Write tests for TaskTamer"),
-//            TaskItem(title: "Redesign Coffeecule")
-//        ]
-//    }
-//    
-//    private func makeSUT(withTasks tasks: [TaskItem]) -> TaskManager {
-//        let sut = TaskManager()
-//        sut.tasks = tasks
-//        return sut
-//    }
-//}
+final class TaskManagerTests: XCTestCase {
+    func test_sort_changesSortStatusToNewStatus() {
+        let sut = makeSUT(withTasks: existingTasks())
+        let taskToSort = sut.tasks.first!
+        sut.sort(taskToSort, to: .afternoon)
+        var sortedTask = taskToSort
+        sortedTask.sortStatus = .afternoon
+        XCTAssertTrue(sut.tasks.contains(where: { $0 == sortedTask}))
+    }
+    
+    // MARK: Helper Methods
+    
+    private func existingTasks() -> [TaskItem] {
+        return [
+            TaskItem(title: "Write tests for TaskTamer", startDate: makeDate(hour: 8, minute: 30), endDate: makeDate(hour: 9, minute: 0)),
+            TaskItem(title: "Redesign Coffeecule", startDate: makeDate(hour: 9, minute: 30), endDate: makeDate(hour: 9, minute: 45))
+        ]
+    }
+    
+    private func makeSUT(withTasks tasks: [TaskItem]) -> TaskManager {
+        let sut = TaskManager()
+        sut.tasks = tasks
+        return sut
+    }
+    
+    private func makeDate(hour: Int, minute: Int) -> Date {
+        Calendar.autoupdatingCurrent.date(bySettingHour: hour, minute: minute, second: 0, of: Date())!
+    }
+}
